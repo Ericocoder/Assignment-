439f89d5d18e2c0d4d8c44262f959ff777f37ac8
Ericocoder <muchuieric07@gmail.com>
2026-09-20 17:07:26 +0300

    Add files via upload

---
+++ week 1.c
@@ -0,0 +1,14 @@
+//Eric muchui 
+//CT100/G/30741/26
+#include <stdio.h>
+int main()
+{
+    printf("Unit Offered This semester:\n");
+    printf("1. Introduction to programming\n");
+    printf("2. Data structures\n");
+    printf("3.Computer Networks\n");
+    printf("4. Database systems \n");
+    printf("5. Software Engineering\n");
+    return 0;
+}
+   
