7bb8b55aae218dfea13716740fb5d3e4f214bd8d
Ericocoder <muchuieric07@gmail.com>
2026-09-20 18:26:57 +0300

    Add files via upload

---
+++ week 1_2.c
@@ -0,0 +1,17 @@
+//Eric muchui 
+//CT100/G/30741/26
+#include <stdio.h>
+int main(){
+float height = 5.5;
+int bank_bal = 5000;
+char phone_no[11] = "142221992";
+printf("Enter your height \t");
+scanf("%f",&height);
+printf("Enter your bank blance \t");
+scanf("%i",&bank_bal);
+printf("Enter your height");
+scanf("%s",&phone_no);
+printf("Your height is %.1f\n",height);
+printf("And your bank balance is %i\n",bank_bal);
+printf("Your phone number is %s\n",phone_no);
+}
