497e8a80ecb43066f35b9709111652638a7dd8da
Ericocoder <muchuieric07@gmail.com>
2026-09-24 15:15:27 +0300

    Add files via upload

---
+++ week 2 .1.c
@@ -0,0 +1,16 @@
+// Eric muchui 
+//cT100/G/30741/26
+# include<stdio.h>
+int main(){
+
+double pi= 3.142;
+float R=4.5;
+float H=5.4;
+printf("enter radiusR\t");
+scanf("%f",&R);
+ printf("enter heightH\t");
+ scanf("%f",&H);
+ double volume=pi*R*R*H;
+ printf( "volume=%.3f",volume);
+ 'return0';
+ }
